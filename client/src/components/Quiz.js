2bb550524466e3a6a6b9c8b5b120bd1c11017979
nupur6299 <snupur604@gmail.com>
2024-11-28 23:47:57 +0530

    client

---
+++ client/src/components/Quiz.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Quiz() {
+  return (
+    <div className="container">
+      <h1 className="title text-light">Quiz Application</h1>
+      <div className="grid">
+        <button className="btn prev">Prev</button>
+        <button className="btn next">Next</button>
+      </div>
+    </div>
+  );
+}
